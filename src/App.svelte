ec9172c12f06fd2024147e7d03555013d23151be
Colin Bate <colin@colinbate.com>
2020-04-16 15:51:14 -0300

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+import Part1 from './Part1.svelte';
+</script>
+
+<main>
+  <h1>Puzzle Quest</h1>
+  <Part1/>
+</main>
+
+<style>
+  main {
+    padding: 1rem;
+    margin: 0 auto;
+  }
+
+  h1 {
+    margin-top: 0;
+    text-align: center;
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: 960px;
+    }
+  }
+</style>
---
+++ src/Part1.svelte
@@ -0,0 +1,69 @@
+<style>
+ul > li {
+  margin: 0.25rem 0;
+}
+.puzzle {
+  background-color: hsl(15, 100%, 50%);
+  color: #fff;
+  width: 100vw;
+  margin: 0;
+  left: 50%;
+  position: relative;
+  transform: translateX(-50%);
+  padding: 1rem 2rem;
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+pre {
+  margin: 0;
+}
+</style>
+<p>Hello intrepid puzzler!</p>
+
+<p>
+  My name is Rhyd, and I am trying to make my way to Earth. My people love visiting you, however, it is my first time and I seem to have lost my way. I have been in periodic contact with my own space command, but transmission quality is not great, and messages take forever to receive. Perhaps you can help me make sense of my last message and find my way to your beautiful planet.
+</p>
+
+<p>As you will see, some of the message is garbled. Also, my people are very logical, but not the most straightforward of beings, so the message may seem a bit odd, I apologize if it gives you trouble. I think my colleagues back home have referenced some Earth data as part of their cryptic message, probably to give me a hard time because I haven't visited before. I'm hoping that you can make more sense of it than I can.</p>
+
+<blockquote>
+<p>Hello Rhyd! Four of your colleagues have just come in with information to help you on your way. Or at least that is what they've said, I'm not sure what that information is. They are all wearing Earth clothing with different colors. Each is wearing a hat, shirt, and a pair of pants. They each came in one at a time, showcasing their clothing, although I'm not sure the significance.</p>
+
+<ul>
+  <li>No shirts are the same color, two of the hats were the same color, as were two of the pants, although not the same color.</li>
+  <li>No single colleague was wearing two articles of the same color.</li>
+  <li>Caltor came in last wearing green pants.</li>
+  <li>The four were, in some order, Paxon, the only one wearing white pants, the only one wearing a red hat and the first through the door (who wore a yellow shirt).</li>
+  <li>Lipgrum entered just after Paxon, and was not wearing red or green.</li>
+  <li>Trum was wearing the same color hat as the 3rd through the door.</li>
+  <li>The black garments were a hat and a shirt, and there was also a single yellow pair of pants.</li>
+  <li>PV JAMHYJ EBHB GOTB SPR PV VPB IVWGMPBR YAB IVOVHJ GOTB SPR HBR.</li>
+  <li>Each color appeared exactly twice.</li>
+</ul>
+</blockquote>
+
+<p>I'm not sure how useful it will be to know what colors everyone is wearing, but knowing them, it will be symbolic in some way with Earth. They've always joked about trying to be taken to your leaders, but I don't think any of them have actually had the nerve to do so. In addition to the message above, I also received this stream of characters which I originally thought might have been noise, but I feel like it may somehow be part of the overall message. It is possible it is somewhat scrambled as well.</p>
+<div class="puzzle">
+<pre>
+K E N G F H R C Y F W Y
+C A F A S N I L R E B V
+G L F B W Y C I A R T F
+R E C O I M L B G D F O
+N K R N F P U R N Y I Y
+N S Y M C D B E U F I G
+I R H L A H E V H U U R
+L C A P F N A I E S F O
+L R E S L F Y L A E S F
+A S A L P F M L Y S V F
+T A I N O T S E K E N D
+F M L O F O D E M I L D
+</pre>
+</div>
+<p>
+  I do hope that you can help me.
+</p>
+
+<p>Truly,<br>
+Rhyd</p>
