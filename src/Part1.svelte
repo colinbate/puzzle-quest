e2780613826fc0c833ed4bc7d10c863644716f33
Colin Bate <colin@colinbate.com>
2020-04-16 22:59:19 -0300

    Fix grammar and clarity

--- src/Part1.svelte
+++ src/Part1.svelte
@@ -18,31 +18,31 @@ pre {
   margin: 0;
 }
 </style>
-<p>Hello intrepid puzzler!</p>
+<p>Hello, intrepid puzzler!</p>
 
 <p>
-  My name is Rhyd, and I am trying to make my way to Earth. My people love visiting you, however, it is my first time and I seem to have lost my way. I have been in periodic contact with my own space command, but transmission quality is not great, and messages take forever to receive. Perhaps you can help me make sense of my last message and find my way to your beautiful planet.
+  My name is Rhyd, and I am trying to make my way to Earth. My people love visiting you; however, it is my first time, and I seem to have lost my way. I have tried to contact my own space command. Transmission quality is not high, and messages take forever to receive. Perhaps you can help me make sense of the last transmission and find my way to your beautiful planet.
 </p>
 
-<p>As you will see, some of the message is garbled. Also, my people are very logical, but not the most straightforward of beings, so the message may seem a bit odd, I apologize if it gives you trouble. I think my colleagues back home have referenced some Earth data as part of their cryptic message, probably to give me a hard time because I haven't visited before. I'm hoping that you can make more sense of it than I can.</p>
+<p>As you will see, some parts of the message are garbled. My people are very logical but not very straightforward. We like to speak in riddles at times. I apologize if this makes things more complicated. I'm hoping that you can make more sense of it than I can.</p>
 
 <blockquote>
-<p>Hello Rhyd! Four of your colleagues have just come in with information to help you on your way. Or at least that is what they've said, I'm not sure what that information is. They are all wearing Earth clothing with different colors. Each is wearing a hat, shirt, and a pair of pants. They each came in one at a time, showcasing their clothing, although I'm not sure the significance.</p>
+<p>Hello Rhyd! Four of your colleagues have just come in with information to help you on your way. Or at least that is what they've said, I'm not sure what that information is, it must be something about their wardrobe. They are all wearing Earth clothing with different colors. Each is wearing a hat, a shirt, and a pair of pants. They each came in one at a time, showcasing their clothing like a show.</p>
 
 <ul>
-  <li>No two shirts were the same color, however, two of the hats were the same color, as were two of the pairs of pants.</li>
+  <li>No two shirts were the same color; however, two of the hats were the same color, as were two of the pairs of pants.</li>
   <li>No single colleague was wearing two articles of the same color.</li>
   <li>Caltor came in last wearing green pants.</li>
   <li>The four were, in some order, Paxon, the only one wearing white pants, the only one wearing a red hat and the first through the door (who wore a yellow shirt).</li>
-  <li>Taplaic entered just after Paxon, and was not wearing red or green.</li>
+  <li>Taplaic entered just after Paxon and was not wearing red or green.</li>
   <li>Staal was wearing the same color hat as the 3rd through the door.</li>
   <li>The black garments were a hat and a shirt, and there was also a single yellow pair of pants.</li>
   <li>PV JAMHYJ EBHB GOTB SPR PV VPB IVWGMPBR YAB IVOVHJ GOTB SPR HBR.</li>
-  <li>Each color appeared exactly twice.</li>
+  <li>Each color appeared precisely two times.</li>
 </ul>
 </blockquote>
 
-<p>I'm not sure how useful it will be to know what colors everyone is wearing, but knowing them, it will be symbolic in some way with Earth. They've always joked about trying to be taken to your leaders, but I don't think any of them have actually had the nerve to do so. In addition to the message above, I also received this stream of characters which I originally thought might have been noise, but I feel like it may somehow be part of the overall message. It is possible it is somewhat scrambled as well.</p>
+<p>I'm not sure how useful it will be to know what colors everyone is wearing, but knowing my colleagues, it will be meaningful and symbolic. They are always joking about the places they've visited and trying to be taken to your Earth leaders. I don't think any of them have actually had the nerve to do so. At the end of the last transmission, I also received this stream of characters. I initially thought it might have been noise, but I feel like it may somehow be part of the overall message. It may be somewhat scrambled as well.</p>
 <div class="puzzle">
 <pre>
 K E N G F H R C Y F W Y
