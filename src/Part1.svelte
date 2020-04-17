a926ea40fec88a6cefd16ffc70ed4845b4544942
Colin Bate <colin@colinbate.com>
2020-04-17 11:14:52 -0300

    Use a numbered list of clues.

--- src/Part1.svelte
+++ src/Part1.svelte
@@ -29,7 +29,7 @@ pre {
 <blockquote>
 <p>Hello Rhyd! Four of your colleagues have just come in with information to help you on your way. Or at least that is what they've said, I'm not sure what that information is, it must be something about their wardrobe. They are all wearing Earth clothing of different colors (one of which was blue). Each is wearing a hat, a shirt, and a pair of pants. They each came in one at a time, showcasing their clothing like a show.</p>
 
-<ul>
+<ol>
   <li>No two shirts were the same color; however, two of the hats were the same color, as were two of the pairs of pants.</li>
   <li>No single colleague was wearing two articles of the same color.</li>
   <li>Caltor came in last wearing green pants.</li>
@@ -38,7 +38,7 @@ pre {
   <li>Staal was wearing the same color hat as the 3rd through the door.</li>
   <li>MCG QABTL TAKMCWHY OGNG B CBM BHJ B DCWNM, BHJ MCGNG OBD BADK B DWHYAG PGAAKO SBWN KR SBHMD.</li>
   <li>Each color appeared precisely two times.</li>
-</ul>
+</ol>
 </blockquote>
 
 <p>I'm not sure how useful it will be to know what colors everyone is wearing, but knowing my colleagues, it will be meaningful and symbolic. They are always joking about the places they've visited and trying to be taken to your Earth leaders. I don't think any of them have actually had the nerve to do so. At the end of the last transmission, I also received this stream of characters. I initially thought it might have been noise, but I feel like it may somehow be part of the overall message. It may be somewhat scrambled as well.</p>
